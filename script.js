d1eea5ed4f4a1715310b48dbf582ba6a1ee67833
Vitor Vouvouloudas de Moraes <vouvouloudas1@gmail.com>
2023-10-08 16:47:39 -0300

    Changes to be committed: 	modified:   modelos.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 
 const button = document.querySelector("button")
-const modal = document.querySelector("dialog")
+const modal = document.querySelector("#modal")
+const modal2 = document.querySelector("#modal2")
+const modal3 = document.querySelector("#modal3")
+const modal4 = document.querySelector("#modal4")
+const modal5 = document.querySelector("#modal5")
+const modal6 = document.querySelector("#modal6")
+const modal7 = document.querySelector("#modal7")
+const modal8 = document.querySelector("#modal8")
+const modal9 = document.querySelector("#modal9")
 buttonClose = document.querySelector("dialog button")
 
 button.onclick = function (){
   modal.showModal()
+  modal2.showModal()
+}
+
+
+buttonClose.onclick = function (){
+  modal2.close()
 }
 
 buttonClose.onclick = function (){
